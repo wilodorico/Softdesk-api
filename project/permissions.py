62afe8d61d52ab59fadb9e1a22777563d96fccb0
wilfried <wilfried.odorico@gmail.com>
2025-04-18 17:18:18 +0200

    add method utils get_project_id_from_kwargs for get project_id if nested route or not and add condition IsAuthor permission for check if user add contributor has_author

--- project/permissions.py
+++ project/permissions.py
@@ -4,9 +4,18 @@ from rest_framework import permissions
 from project.models import Project
 
 
+def get_project_id_from_kwargs(view):
+    """
+    Helper function to get the project ID from the view's kwargs.
+    """
+    if "project_pk" in view.kwargs:
+        return view.kwargs.get("project_pk")
+    return view.kwargs.get("pk")
+
+
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """
-    allows the author to make changes.
+    Allows the author to make changes.
     """
 
     message = "You are not authorized to perform this action."
@@ -14,6 +23,15 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+
+        if view.basename == "project-contributors":
+            project_id = get_project_id_from_kwargs(view)
+            if project_id is None:
+                return False
+
+            project = get_object_or_404(Project, pk=project_id)
+            has_author = project.author == request.user
+            return has_author
 
         return obj.author == request.user
 
@@ -26,7 +44,7 @@ class IsContributor(permissions.BasePermission):
     message = "You are not authorized to perform this action."
 
     def has_permission(self, request, view):
-        project_id = view.kwargs.get("project_pk") if view.basename == "project-issues" else view.kwargs.get("pk")
+        project_id = get_project_id_from_kwargs(view)
 
         if view.basename == "projects" and view.action == "list":
             return request.user.is_authenticated
@@ -35,7 +53,6 @@ class IsContributor(permissions.BasePermission):
             return False
 
         project = get_object_or_404(Project, pk=project_id)
-
         has_contributor = project.contributors.filter(user=request.user).exists()
 
         return has_contributor
