adf086f80509c68fc6dd79a58148734c84af1bd0
wilfried <wilfried.odorico@gmail.com>
2025-04-18 15:59:25 +0200

    create permission IsContributor

--- project/permissions.py
+++ project/permissions.py
@@ -1,4 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions
+
+from project.models import Project
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
@@ -13,3 +16,26 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.author == request.user
+
+
+class IsContributor(permissions.BasePermission):
+    """
+    Allows contributors to access
+    """
+
+    message = "You are not authorized to perform this action."
+
+    def has_permission(self, request, view):
+        project_id = view.kwargs.get("project_pk") if view.basename == "project-issues" else view.kwargs.get("pk")
+
+        if view.basename == "projects" and view.action == "list":
+            return request.user.is_authenticated
+
+        if project_id is None:
+            return False
+
+        project = get_object_or_404(Project, pk=project_id)
+
+        has_contributor = project.contributors.filter(user=request.user).exists()
+
+        return has_contributor
