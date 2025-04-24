a71e8f4226c52890917f266fa6a9efaae20e2324
wilfried <wilfried.odorico@gmail.com>
2025-04-24 18:00:05 +0200

    Add password field to UserSerializer and implement update method for password handling

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -8,9 +8,23 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, validators=[validate_password])
+
     class Meta:
         model = User
-        fields = ("id", "username", "age", "can_be_contacted", "can_data_be_shared")
+        fields = ("id", "username", "age", "can_be_contacted", "can_data_be_shared", "password")
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
