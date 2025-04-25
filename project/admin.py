20e45cdadb805173b07c1ff95161e306836f1d56
wilfried <wilfried.odorico@gmail.com>
2025-04-25 10:30:26 +0200

    Register Comment and Issue models in the Django admin

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from project.models import Contributor, Project
+from project.models import Comment, Contributor, Issue, Project
 
 admin.site.register(Project)
 admin.site.register(Contributor)
+admin.site.register(Issue)
+admin.site.register(Comment)
